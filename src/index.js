9ba8f82b8a486231c82935b3c0f235852eb2fa59
aderito.xavier <aderito_xavier@sapo.pt>
2019-02-10 13:28:31 +0000

    main structure built

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App';
+import FavoriteRecipeList from './components/FavoriteRecipeList';
+import rootReducer from './reducers/usersReducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// { Provider } permite que todos os elementos dentro do mesmo "nested", tenham acesso à store
+import { Provider } from 'react-redux';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Permite criar uma { store }
+import { createStore } from 'redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+const store = createStore(rootReducer);
+
+
+
+ReactDOM.render(
+  <Provider store={ store }>
+    <BrowserRouter>
+      <Switch>
+        <Route exact={true} path='/' component={App} />
+        <Route exact={true} path='/favorites' component={FavoriteRecipeList} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
+    document.getElementById('root')
+  );

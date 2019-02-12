16080691bd3669651e40594a5810f79a6e1d2787
aderito.xavier <aderito_xavier@sapo.pt>
2019-02-12 20:04:32 +0000

    filter completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,70 +1,123 @@
-    import React, { Component } from 'react';
-    import { connect } from 'react-redux';
-    import { fetchUsers } from '../actions/userActions';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchUsers } from '../actions/userActions';
 
-    class App extends Component {
-        constructor(props) {
-            super(props);
-            this.handleSubmit = this.handleSubmit.bind(this);
-            this.getUsers = this.getUsers.bind(this);
-            this.state = {
-                name: '',
-                username: '',
-                email: '',
-                phone: '',
-                website: '',
-            };
-        
 
-        }
-        handleSubmit(event) {
-            console.log('props: ', this.props);
-            this.getUsers();
-            
-            event.preventDefault();
-        }
+function searchingForName(name, username, email, website, phone) {
+    return function(x) {
+        return ((x.name.toLowerCase().includes(name.toLowerCase()) || !name) && 
+                (x.username.toLowerCase().includes(username.toLowerCase()) || !username) &&
+                (x.email.toLowerCase().includes(email.toLowerCase()) || !email) && 
+                (x.website.toLowerCase().includes(website.toLowerCase()) || !website) && 
+                (x.phone.toLowerCase().includes(phone.toLowerCase()) || !phone)
+        )
+    }
+}
+
+
+
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.getUsers = this.getUsers.bind(this);
+        this.nameSearchHandler = this.nameSearchHandler.bind(this);
+        this.usernameSearchHandler = this.usernameSearchHandler.bind(this);
+        this.emailSearchHandler = this.emailSearchHandler.bind(this);
+        this.phoneSearchHandler = this.phoneSearchHandler.bind(this);
+        this.websiteSearchHandler = this.websiteSearchHandler.bind(this);
+        this.state = {
+            users: [],
+            name: '',
+            username: '',
+            email: '',
+            phone: '',
+            website: '',
+        };
+    
+
+    }
+    handleSubmit(event) {
+        console.log('props: ', this.props);
+        this.getUsers();
         
-        getUsers() {
-            
-            return this.props.fetchUsers({
-                name: this.state.name,
-                username: this.state.username,
-                email: this.state.email,
-                phone: this.state.phone,
-                website: this.state.website,
-            });
-            
-        }
+        event.preventDefault();
+    }
+    
+    getUsers() {
         
-        componentDidMount() {
-            this.getUsers();
-        }
+        return this.props.fetchUsers();
         
-        render() {
-            
-            
-            
-            return (
-                <div>
-                    <form onSubmit={this.handleSubmit}>
-                        <label>
+    }
+    
+    componentDidMount() {
+        this.getUsers();
+    }
+
+    nameSearchHandler(event) {
+        this.setState({ name: event.target.value });
+    }
+
+    usernameSearchHandler(event) {
+        this.setState({ username: event.target.value });
+    }
+
+    emailSearchHandler(event) {
+        this.setState({ email: event.target.value });
+    }
+
+    websiteSearchHandler(event) {
+        this.setState({ website: event.target.value });
+    }
+
+    phoneSearchHandler(event) {
+        this.setState({ phone: event.target.value });
+    }
+
+    render() {
+        const users = this.props;
+        console.log('yay', users);
+        
+        
+        return (
+            <div>
+                <form>
+                    <label>
                         Name:
-                        <input type="text" value={this.state.name} onChange={event => this.setState({ name: event.target.value })} />
-                        <input type="text" value={this.state.username} onChange={event => this.setState({ username: event.target.value })} />
-                        <input type="text" value={this.state.email} onChange={event => this.setState({ email: event.target.value })} />
-                        <input type="text" value={this.state.phone} onChange={event => this.setState({ phone: event.target.value })} />
-                        <input type="text" value={this.state.website} onChange={event => this.setState({ website: event.target.value })} />
-                        </label>
-                        <input type="submit" value="Submit" />
-                    </form>
+                        <input type="text" value={this.state.name} onChange={this.nameSearchHandler} />
+                    </label>
 
-                    <ul className="list-group">
-                {
-                        this.props.users.map(user => {
+                    <label>
+                        Username:
+                        <input type="text" value={this.state.username} onChange={this.usernameSearchHandler} />
+                    </label>
+
+                    <label>
+                        Email:
+                        <input type="text" value={this.state.email} onChange={this.emailSearchHandler} />
+                    </label>
+
+                    <label>
+                        Website:
+                        <input type="text" value={this.state.website} onChange={this.websiteSearchHandler} />
+                    </label>
+
+                    <label>
+                        Phone:
+                        <input type="text" value={this.state.phone} onChange={this.phoneSearchHandler} />
+                    </label>
+
+                    {
+                        this.props.users.filter(searchingForName(this.state.name, this.state.username,
+                        this.state.email, this.state.website, this.state.phone)).map(user => {
                             return (
                                 <li key={user.id} className="list-group-item">
                                     <div className="list-item">
-                                        {user.name}
+                                        <h3>{user.username}</h3>
+                                        <p>{user.name}</p>
+                                        <p>{user.email} {user.website} {user.phone}</p>
                                     </div>
                                     <div className="list-item right-button"
                                     >
@@ -74,11 +127,13 @@
                             )
                         })
                     }
-                </ul>
-                </div>
-            )
-        }
+                    
+                </form>
+
+            </div>
+        )
     }
+}
 
 
 function mapStateToProps(state) {
